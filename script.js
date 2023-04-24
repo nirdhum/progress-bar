0f2b06766bd8078f849ba97b2bc63c37ee48825b
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-04-24 12:48:35 +0530

    Added script.jss file with a littile bit of code

---
+++ script.js
@@ -0,0 +1,5 @@
+const progress = document.getElementById('progress')
+const previous = document.getElementById('previous')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
